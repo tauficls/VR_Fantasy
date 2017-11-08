542d71c3040e7a04e7285245a17feefbc9892c81
TauficLeonardoS <13514022@std.stei.itb.ac.id>
2017-11-08 19:09:52 +0700

    add base api

---
+++ app/src/main/java/com/taufic/vr_fantasy/Utils/LoadingViewHolder.java
@@ -0,0 +1,14 @@
+package com.taufic.vr_fantasy.Utils;
+
+import android.view.View;
+
+/**
+ * Created by taufic on 11/8/2017.
+ */
+
+public class LoadingViewHolder extends RecyclerView.ViewHolder {
+
+    public LoadingViewHolder(View itemView) {
+        super(itemView);
+    }
+}
